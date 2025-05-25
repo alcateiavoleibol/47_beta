8dc5379a65e9bc76dcbfc3ab463d2f46735523f4
alcateiavoleibol <alcateiavoleibol2023@gmail.com>
2025-05-25 20:23:32 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 
 // Script simples para interação futura
 console.log("Site Forty Seven carregado.");
+
+
+document.getElementById("menu-toggle").addEventListener("click", function() {
+  document.querySelector("nav ul").classList.toggle("show");
+});
+
